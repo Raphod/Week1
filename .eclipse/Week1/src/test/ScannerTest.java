2bf3df3c751f367f50e11843f1183627914e270e
Raphod <odedeyiralph@gmail.com>
2023-02-01 22:20:41 -0700

    "first commit"

---
+++ .eclipse/Week1/src/test/ScannerTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import java.util.Scanner; 
+
+public class ScannerTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	Scanner x = new Scanner(System.in);
+	int houseAddress;
+	System.out.println("House address");
+	houseAddress = x. nextInt();
+	System.out.println("houseAddress is: " + houseAddress);
+	sc.close();
+	} 
+
+}
